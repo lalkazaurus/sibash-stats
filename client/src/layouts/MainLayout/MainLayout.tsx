4806683070dfacf45012917cbb7e8bebe2f046b5
lalkazaurus <indiraptor63983@gmail.com>
2026-08-13 13:25:00 +0300

    Error boundary was added

--- client/src/layouts/MainLayout/MainLayout.tsx
+++ client/src/layouts/MainLayout/MainLayout.tsx
@@ -2,12 +2,16 @@ import { Outlet } from "react-router-dom"
 import styles from "./MainLayout.module.scss"
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
+import { ErrorBoundary } from "react-error-boundary"
+import FallBack from "../FallBack/FallBack"
 
 export default function MainLayout() {
     return <div className={styles.wrapper}>
         <Header/>
         <main className={styles.content}>
-            <Outlet/>
+            <ErrorBoundary FallbackComponent={FallBack}>
+                <Outlet/>
+            </ErrorBoundary>
         </main>
         <Footer/>
     </div>
